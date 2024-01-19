36404539ca0bbadcf8d5031cb17449824866e3db
Lithpth <disasterpower6141@gmail.com>
2024-01-18 17:19:44 -0700

    PLEASE COMMIT

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// current year
+document.getElementById('currentYear').textContent = new Date().getFullYear();
+// lastModified
+document.getElementById('lastModified').textContent = 'Last Modified: ' + document.lastModified;
+
