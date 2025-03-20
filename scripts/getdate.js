f6d9024318741450f555d26bac96de6a7ea8f80e
Lithpth <disasterpower6141@gmail.com>
2025-03-20 12:32:11 -0700

    css and js scripts

---
+++ scripts/getdate.js
@@ -0,0 +1,4 @@
+// current year
+document.getElementById('currentYear').textContent = new Date().getFullYear();
+// lastModified
+document.getElementById('lastModified').textContent = 'Last Modified: ' + document.lastModified;
